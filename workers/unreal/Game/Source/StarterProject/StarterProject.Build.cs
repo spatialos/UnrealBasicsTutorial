a3a23783c9c82ab5bb9295b158c078075e672996
improbable-valentyn <32096431+improbable-valentyn@users.noreply.github.com>
2018-05-16 12:00:12 +0100

    Upgrade to SpatialOS 13.0.0 (#1)
    * Upgrade to SpatialOS 13.0.0
    
    * Update links in the README
    
    * Ignore generated files
    
    * Ignore plugin binaries

--- workers/unreal/Game/Source/StarterProject/StarterProject.Build.cs
+++ workers/unreal/Game/Source/StarterProject/StarterProject.Build.cs
@@ -7,16 +7,10 @@ using System.IO;
 
 public class StarterProject : ModuleRules
 {
-#if !WITH_FORWARDED_MODULE_RULES_CTOR
-	// Backwards compatibility with Unreal 4.15
-	public StarterProject(TargetInfo Target)
-#else
-	// Unreal 4.16+
 	public StarterProject(ReadOnlyTargetRules Target) : base(Target)
-#endif
 	{
-        PCHUsage = ModuleRules.PCHUsageMode.UseExplicitOrSharedPCHs;
-				bFasterWithoutUnity = true;
+		PCHUsage = ModuleRules.PCHUsageMode.UseExplicitOrSharedPCHs;
+		bFasterWithoutUnity = true;
 
 		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "SpatialOS"});
 	}
